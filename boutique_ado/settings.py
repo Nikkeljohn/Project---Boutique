d5d497b06effff5d31ee8662d7c173de02bbf7c2
Nikkeljohn <nikkeljohn120@gmail.com>
2023-07-10 19:19:28 +0000

    added basic functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '8000-nikkeljohn-projectbouti-iccthrs1xz5.ws-eu101.gitpod.io']
+ALLOWED_HOSTS = [ '8000-nikkeljohn-projectbouti-vvh1ezo0fkx.ws-eu101.gitpod.io' ]
 
 
 # Application definition
@@ -168,3 +168,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+# Stripe
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
