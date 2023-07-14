493716317d2348f268e91cf51f1c4f7d749b2427
Nikkeljohn <nikkeljohn120@gmail.com>
2023-07-14 09:55:07 +0000

    added class methods ton weh handeler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-nikkeljohn-projectbouti-vvh1ezo0fkx.ws-eu101.gitpod.io' ]
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Application definition
 INSTALLED_APPS = [
@@ -176,3 +178,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
